269d3fe4a8afa2063f1b5d1c6622926601bd2dbb
nikhil <21bd1a1283itb@gmail.com>
2024-10-25 13:43:14 +0530

    errors

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default async function Home() {  
   // const data = await prisma.weatherData.findMany();
   const res  = await fetch(
-    `api.openweathermap.org/data/2.5/forecast/daily?lat={17.366}&lon=${78.476}&cnt=${2}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`);
+    `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${17.366}&lon=${78.476}&cnt=${2}&appid=${process.env.NEXT_PUBLIC_OPENWEATHER_API_KEY}`);
   const data = await res.json();
   // console.log(data);
   return (
